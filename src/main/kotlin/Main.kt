776e3dcf46932fb387ba6b335a9bcc1d3f07ef26
mr11011 <m.elrouby@nu.edu.eg>
2022-08-04 21:59:25 +0200

    Revert "Version <<3>>"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 fun main(args: Array<String>) {
-    val x=11
-    val y=20
+    //This my third change
+    println("Enter your numbers: ")
+    val sc=Scanner(System.`in`)
+    val x=sc.nextInt()
+    val y=sc.nextInt()
+
     val z=x+y
     println("Sum=" + z)
 
